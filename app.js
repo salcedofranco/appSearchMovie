fd8f48b679e2eeaa4fb33c33bcdab269184415e7
franco <fcedo13@gmail.com>
2020-02-10 17:24:23 -0300

    comentarios de server

--- app.js
+++ app.js
@@ -30,11 +30,16 @@ app.get('/search' , (req, res) =>{
     res.render('search');
 });
 
+
+// PUERTO LOCALHOST 3000
 //app.listen(3000, () =>{
    // console.log('Server escuchando en puerto 3000.');
 
 //});//
 
+
+
+//ESTE PUERTO ES PARA HEROKU
 app.listen(process.env.PORT, () => {
     console.log("Puerto 3000 funcionando!");
 });
